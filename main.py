46018b09b3e688e016d888789c41f0f5649927d8
ShahabSH94 <Shahab.shabani94@gmail.com>
2023-05-30 19:16:36 +0330

    init commit 2

---
+++ main.py
@@ -0,0 +1,102 @@
+import keyboard
+from llama_cpp import Llama
+from dotenv import dotenv_values
+import pyperclip
+import winsound
+env_vars = dotenv_values('.env')
+while True:
+    print("""autocomplete Anything
+Select mode
+1.symbol document mode
+2.clipboard mode
+3.help
+    """)
+    usrcmd = input(">>")
+    if usrcmd in ["1","2","3"]:
+        usrselection = usrcmd
+        if usrselection == "1":
+            try:
+                documentpath = env_vars['document-path']
+            except Exception as e:
+                print(f"{e}")
+            break
+        if usrselection == "2":
+            break
+        if usrselection == "3":
+            print(r"""
+symbol document mode:
+in this mode enter the absolute path to your document for example C:\Users\file.txt in .env file
+then on wherever you want completion just put the symbol(default=$$) on there then for example:
+def helloworld()
+    print$$
+then press shift+ctrl hopefully you will get something like ("Hello, world!") then remove the symbol if you dont want any more completions on that line
+if you didnt like what you got just ctrl+z to undo
+some programs lock the file while the file is opened in that program so currently they're not supported also some other programs might not support undo
+if file was edited from another program in that case you might want to use clipboard mode
+
+clipboard mode:
+in this mode copy the text you want completion for then press shift+ctrl to get completion if autopaste in .env file is set to true
+then the completion will get pasted at your cursor onto whatever field its on if not you will hear a beep indicating that your completion is ready
+""")      
+    else:
+        print("wrong entry")
+
+modelpath = env_vars['Model-path']
+CTX_MAX = int(env_vars['Max-Context'])
+n_threads = int(env_vars['n_threads'])
+autopaste = False if (env_vars['autopaste']).lower() == 'false' else True
+maxtokens = int(env_vars['max_tokens'])
+try:
+    symbol = env_vars['symbol']
+except:
+    symbol = "$$"
+temperature = float(env_vars['temperature'])
+repeat_penalty=float(env_vars['repeat_penalty'])
+llm = Llama(model_path=modelpath,n_ctx=CTX_MAX,n_threads=n_threads)
+promptslice = int(CTX_MAX - CTX_MAX/3)
+if usrselection == "1":
+    file = documentpath
+def GetCompleterResponse(prompt:str):
+    output = llm(prompt=prompt,temperature=temperature,repeat_penalty=repeat_penalty,max_tokens=maxtokens)
+    return output['choices'][0]['text'].strip()#,top_p=top_p,top_k=top_k)
+def get_lines_within_max_length(lines, max_length):
+    total_length= 0
+    if usrselection == "1":
+        lines = [line.split(symbol)[0] for i, line in enumerate(lines) if symbol in line for line in lines[:i+1]]
+    for i in range(len(lines)-1, -1, -1):
+        total_length += len(lines[i])
+        if total_length > max_length:
+            return lines[i+1:]
+    return lines
+def get_prediction():
+    print('get_prediction Key was pressed.')
+    if usrselection == "1":
+        with open(file, "r") as f:
+            lines = f.read().split("\n")
+        symbolcount =0
+        for i in lines:
+            if symbol in i:
+                symbolcount+=1
+            if symbolcount > 1:
+                print("symbol in multiple lines make sure only one symbol is in the document")
+                break
+        if symbolcount == 1:
+            prevlines = get_lines_within_max_length(lines,promptslice)
+            prompt = (lambda lines: "\n".join([line for line in lines]))(prevlines).replace(symbol,"")
+            response = GetCompleterResponse(prompt)
+            new_content = '\n'.join([(line[:line.index(symbol)] + response + line[line.index(symbol):]) if symbol in line else line for line in lines])
+            with open(file,'w') as f:
+                f.write(new_content)
+    else:
+        clipboard_data = pyperclip.paste()
+        prompt = get_lines_within_max_length(clipboard_data,promptslice)
+        response = GetCompleterResponse(prompt)
+        pyperclip.copy(response)
+        if autopaste:
+            keyboard.press_and_release("ctrl+v")
+        else:
+            winsound.Beep(450, 200)
+    print("Done")
+
+keyboard.add_hotkey('shift+ctrl', get_prediction)
+keyboard.wait()
