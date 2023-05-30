a45424f0d9a1be9fccc41c700af0729b93cb0fbe
ShahabSH94 <Shahab.shabani94@gmail.com>
2023-05-30 20:59:29 +0330

    Update main.py
    replaced hardcoded help with README.md

--- main.py
+++ main.py
@@ -23,21 +23,8 @@ Select mode
         if usrselection == "2":
             break
         if usrselection == "3":
-            print(r"""
-symbol document mode:
-in this mode enter the absolute path to your document for example C:\Users\file.txt in .env file
-then on wherever you want completion just put the symbol(default=$$) on there then for example:
-def helloworld()
-    print$$
-then press shift+ctrl hopefully you will get something like ("Hello, world!") then remove the symbol if you dont want any more completions on that line
-if you didnt like what you got just ctrl+z to undo
-some programs lock the file while the file is opened in that program so currently they're not supported also some other programs might not support undo
-if file was edited from another program in that case you might want to use clipboard mode
-
-clipboard mode:
-in this mode copy the text you want completion for then press shift+ctrl to get completion if autopaste in .env file is set to true
-then the completion will get pasted at your cursor onto whatever field its on if not you will hear a beep indicating that your completion is ready
-""")      
+            with open("README.md","r") as f:
+                print(f.read())  
     else:
         print("wrong entry")
 
